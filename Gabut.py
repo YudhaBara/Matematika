7db239f42330189b01c6ae1eedd50fea83c8bcfc
YudhaBara <65260237+YudhaBara@users.noreply.github.com>
2020-05-22 06:05:09 +0700

    Add files via upload

---
+++ Gabut.py
@@ -0,0 +1,28 @@
+print ('clear')
+
+def Hitungluas():
+#MENU
+print("--- PROGRAM HITUNG LUAS ---")
+print("1. Persegi")
+print("2. Persegi Panjang")
+print("3. Segitiga")
+print("4. Lingkaran")
+print("0. Tutup")
+#Pilih MENU
+menu = input("Pilih menu :") #input nomor menu
+#Cek MENU Pilihan
+if menu==1:
+persegi()
+elif menu==2:
+persegipanjang
+elif menu==3:
+segitiga)
+elif menu==4:
+lingkaran)
+elif menu==0:
+clearScreen()
+print("bye")
+else:
+print("Menu tidak ditemukan")
+display_menu()
+
