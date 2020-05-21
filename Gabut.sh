c1a5ca2538b1790af0848ac6ceb7b55f6d31efee
YudhaBara <65260237+YudhaBara@users.noreply.github.com>
2020-05-22 06:40:48 +0700

    Add files via upload

---
+++ Gabut.sh
@@ -0,0 +1,79 @@
+clear
+echo  "-------~---C.E.S---~---------"
+echo "creator : ⚔️-YudhaBara-⚔️
+echo "Fb      : ⚔️-Angel La-⚔️
+echo "🤗-Nothing last foreve️r-🤗"
+echo  "-------~---C.E.S---~---------"
+sleep 2
+echo "Stop CTRL+Z"
+sleep 3
+echo "-----------------------------"
+echo  "Kuis Matematika";
+echo  "jawab dengan benar !";
+read -p '10 + 10 = ' userinput1;
+#
+if [ "${userinput1:-}" = "20" ]
+then
+        echo  "BENAR !";
+fi
+if [ "${userinput1:-}" != "20" ]
+then
+        echo  "SALAH !";
+fi
+#
+read -p '20 + 20 = ' userinput1;
+#
+if [ "${userinput1:-}" = "40" ]
+then
+        echo  "BENAR !";
+fi
+if [ "${userinput1:-}" != "40" ]
+then
+        echo  "SALAH !";
+fi
+#
+read -p '30 + 30 = ' userinput1;
+#
+if [ "${userinput1:-}" = "60" ]
+then
+        echo  "BENAR !";
+fi
+if [ "${userinput1:-}" != "60" ]
+then
+        echo  "SALAH !";
+fi
+echo  "Kuis Matematika";
+echo  "sekarang perkalian !";
+read -p '99 x 65 = ' userinput1;
+#
+if [ "${userinput1:-}" = "2145" ]
+then
+        echo  "BENAR !";
+fi
+if [ "${userinput1:-}" != "2145" ]
+then
+        echo  "SALAH !";
+fi
+#
+read -p '100 x 77 = ' userinput1;
+#
+if [ "${userinput1:-}" = "7700" ]
+then
+        echo  "BENAR !";
+fi
+if [ "${userinput1:-}" != "7700" ]
+then
+        echo  "SALAH !";
+fi
+echo "SELESAI"
+echo "sip jangan Pernah menyerah demi suatu yang inginkan"
+sleep 3
+echo "SELAMAT IDUL FIRTI MOHON MAAF LAHIR DAN BATIN"
+sleep 2
+echo "--------------------------"
+echo "  ⚔️Mr.Bara⚔️ "
+echo "--------------------------"
+sleep 2
+echo "Lu jomblo kan ? sama :("
+sleep 5
+echo "Jancok"
